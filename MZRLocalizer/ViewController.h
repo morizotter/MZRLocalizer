3a65b7539161c47a1017b030e4e3d08eafe92ba6
Naoki Morita <namorit@gmail.com>
2014-03-08 00:16:52 +0900

    Initial Commit

---
+++ MZRLocalizer/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  MZRLocalizer
+//
+//  Created by MORITA NAOKI on 2014/03/08.
+//  Copyright (c) 2014年 molabo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ MZRLocalizerTests/MZRLocalizerTests.m
@@ -0,0 +1,34 @@
+//
+//  MZRLocalizerTests.m
+//  MZRLocalizerTests
+//
+//  Created by MORITA NAOKI on 2014/03/08.
+//  Copyright (c) 2014年 molabo. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface MZRLocalizerTests : XCTestCase
+
+@end
+
+@implementation MZRLocalizerTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
